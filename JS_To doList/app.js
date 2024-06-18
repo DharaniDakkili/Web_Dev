f3550dba33d621a34be06984b06867454602051e
Dharani <dakkilidharani@gmail.com>
2024-06-18 15:25:21 +0530

    wd

---
+++ JS_To doList/app.js
@@ -0,0 +1,36 @@
+const users=[];
+const options={weekday:'long',day:'numeric',month:'long',
+}
+let nameContainer =document.getElementById('nameContainer');
+
+renderName =()=>{
+    nameContainer.innerHTML=''
+    users.map((user)=>{
+        let div= document.createElement('div');
+        let p =document.createElement('p');
+        let date=document.createElement('p');
+        div.classList.add('card');
+        p.innerText=user.name;
+        let dateToFormat =new Date(user.date);
+        let fDate=dateToFormat.toLocaleDateString('en',options);
+        date.innerText=fDate;
+        nameContainer.appendChild(div);
+        div.appendChild(p);
+        div.appendChild(date);
+       
+
+
+    })
+
+}
+
+
+
+
+nameUser =()=>{
+    let name=document.getElementById('name');
+    let date=document.getElementById('date');
+    let tempUser= {name:name.value , date:date.value};
+    users.push(tempUser);
+    renderName();
+}
